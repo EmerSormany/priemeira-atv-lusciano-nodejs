5123375d5bc7749f67f184acec366c7c10269f52
Emerson Sormany <sormanyemerson@gmail.com>
2024-11-24 21:09:13 +0000

    feat: criação de servidor

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+const moment = require('moment')
+const http = require('http')
+const fs = require('fs')
 
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {"content-type" : "text/plain"})
+    res.end("Hello, World")
+})
+
+server.listen(3000, () => {
+    console.log(`Servidor iniciado em ${moment().format('DD/MM/YYYY HH:mm')}`);
+    
+    console.log("Servidor rodando em http://localhost:3000");
+})
