f1e43f3e67d5782df32887b6419dd00bfc41d50e
EmerSormany <sormanyemerson@gmail.com>
2024-11-24 18:35:54 -0300

    feat: Resolução de atividade sobre utilizar módulo fs

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ const moment = require('moment')
 const http = require('http')
 const fs = require('fs')
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {"content-type" : "text/plain"})
-    res.end("Hello, World")
+const server = http.createServer((_, res) => {
+    try {
+        const text = fs.readFileSync("ola.txt", "utf8")
+        res.writeHead(200, {"content-type" : "text/plain"})
+        res.end(text)
+      } catch (err) {
+        console.error(err)
+      }
 })
 
 server.listen(3000, () => {
     console.log(`Servidor iniciado em ${moment().format('DD/MM/YYYY HH:mm')}`);
-    
     console.log("Servidor rodando em http://localhost:3000");
 })
